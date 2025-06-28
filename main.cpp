d839c5023de5fbc4a88f9070fab4e0cfd1aba40f
Alex Lu <Alexhonglu@gmail.com>
2025-06-27 22:25:36 -0400

    Created main, A*, PuzzleState, and UCS classes

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>          // for cout printing
+#include <vector>            // for vector
+#include <queue>             // for priority_queue
+#include <unordered_set>     // for unordered_set
+#include <string>            // for string
+#include <sstream>           // for stringstream (toString method)
+#include <chrono>            // for timing
+#include <algorithm>         // for reverse
+
+using namespace std;
+
+int main() {
+    vector<vector<int>> startBoard = {
+        {1, 2, 3},
+        {8, 0, 4},
+        {7, 6, 5}
+    };
+
+    vector<vector<int>> goalBoard = {
+        {1, 2, 3},
+        {8, 0, 4},
+        {7, 6, 5}
+    };
+
+
+    return 0;
+}
