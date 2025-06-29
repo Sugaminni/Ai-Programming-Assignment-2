a2f5ea6fee032e4d8ccecfacbf9c4dc91b6234c9
Alex Lu <Alexhonglu@gmail.com>
2025-06-28 21:00:50 -0400

    Updated PuzzleState.h and PuzzleState.cpp

--- PuzzleState.cpp
+++ PuzzleState.cpp
@@ -5,3 +5,76 @@
 
 using namespace std;
 
+// Constructor to initialize board, parent, and costs
+PuzzleState::PuzzleState(vector<vector<int>> b, PuzzleState* p, int g, int h) {
+    board = b;
+    parent = p;
+    gCost = g;
+    hCost = h;
+    fCost = gCost + hCost;
+}
+
+// Checks if current board matches goal board
+bool PuzzleState::isGoal(const vector<vector<int>>& goalBoard) {
+    return board == goalBoard;
+}
+
+// Prints the puzzle board
+void PuzzleState::print() {
+    for (auto row : board) {
+        for (int tile : row) {
+            cout << tile << " ";
+        }
+        cout << endl;
+    }
+    cout << "---------" << endl;
+}
+
+// Converts board to string for hashing/comparison
+string PuzzleState::toString() {
+    stringstream ss;
+    for (auto row : board) {
+        for (int tile : row) {
+            ss << tile;
+        }
+    }
+    return ss.str();
+}
+
+// Generates all valid next states by moving empty tile
+vector<PuzzleState*> PuzzleState::generateNeighbors() {
+    vector<PuzzleState*> neighbors;
+
+    // Finds location of empty tile (0)
+    int zeroRow, zeroCol;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i][j] == 0) {
+                zeroRow = i;
+                zeroCol = j;
+            }
+        }
+    }
+
+    // Possible move directions: up, down, left, right
+    vector<pair<int, int>> directions = {
+        {-1, 0}, {1, 0}, {0, -1}, {0, 1}
+    };
+
+    for (auto dir : directions) {
+        int newRow = zeroRow + dir.first;
+        int newCol = zeroCol + dir.second;
+
+        // Checks bounds
+        if (newRow >= 0 && newRow < 3 && newCol >= 0 && newCol < 3) {
+            vector<vector<int>> newBoard = board;
+            swap(newBoard[zeroRow][zeroCol], newBoard[newRow][newCol]);
+
+            // Creates new PuzzleState for this move
+            PuzzleState* newState = new PuzzleState(newBoard, this, gCost + 1);
+            neighbors.push_back(newState);
+        }
+    }
+
+    return neighbors;
+}
--- PuzzleState.h
+++ PuzzleState.h
@@ -1,8 +1,33 @@
-//
-// Created by alexh on 6/27/2025.
-//
-
 #ifndef PUZZLESTATE_H
 #define PUZZLESTATE_H
 
-#endif //PUZZLESTATE_H
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class PuzzleState {
+public:
+    vector<vector<int>> board;
+    PuzzleState* parent;
+    int gCost;
+    int hCost;
+    int fCost;
+
+    // Constructor for puzzle state
+    PuzzleState(vector<vector<int>> b, PuzzleState* p = nullptr, int g = 0, int h = 0);
+
+    // Checks if this state is the goal
+    bool isGoal(const vector<vector<int>>& goalBoard);
+
+    // Prints the board
+    void print();
+
+    // Converts board to a string for hashing
+    string toString();
+
+    // Generates all valid next states
+    vector<PuzzleState*> generateNeighbors();
+};
+
+#endif // PUZZLESTATE_H
